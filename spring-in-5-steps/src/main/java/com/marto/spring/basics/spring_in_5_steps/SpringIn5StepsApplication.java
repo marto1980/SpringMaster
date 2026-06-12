5294e9fe7f377c3064e4992680e4c3879bc79aef
Martin Ivanov <marto1980@gmail.com>
2026-06-12 10:30:36 +0300

    Initialize a Spring Project

---
+++ spring-in-5-steps/src/main/java/com/marto/spring/basics/spring_in_5_steps/SpringIn5StepsApplication.java
@@ -0,0 +1,13 @@
+package com.marto.spring.basics.spring_in_5_steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn5StepsApplication.class, args);
+	}
+
+}
