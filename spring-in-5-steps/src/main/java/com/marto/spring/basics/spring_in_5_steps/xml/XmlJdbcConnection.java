1740376f7a79cb8f544dd9a89e52f587150a46dc
Martin Ivanov <marto1980@gmail.com>
2026-06-17 13:30:04 +0300

    Load XML Beans via Annotations and XML Context

--- spring-in-5-steps/src/main/java/com/marto/spring/basics/spring_in_5_steps/xml/XmlJdbcConnection.java
+++ spring-in-5-steps/src/main/java/com/marto/spring/basics/spring_in_5_steps/xml/XmlJdbcConnection.java
@@ -1,6 +1,8 @@
 package com.marto.spring.basics.spring_in_5_steps.xml;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class XmlJdbcConnection {
   public XmlJdbcConnection() {
     System.out.println("JDBC Connection");
