561499565003ee61149df5d9c5f6c3fabc38005c
Martin Ivanov <marto1980@gmail.com>
2026-06-17 19:06:15 +0300

    Simplify Tests with Mockito Annotations

--- mockito-demo/src/test/java/com/marto/mockito/mockito_demo/business/SomeBusinessImplMockTest.java
+++ mockito-demo/src/test/java/com/marto/mockito/mockito_demo/business/SomeBusinessImplMockTest.java
@@ -1,27 +1,39 @@
 package com.marto.mockito.mockito_demo.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.OptionalInt;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessImplMockTest {
+  @Mock private DataService dataServiceMock;
+  @InjectMocks private SomeBusinessImpl businessImpl;
+
   @Test
   void findTheGreatestFromAllData_basicScenario() {
-    DataService dataServiceMock = Mockito.mock(DataService.class);
     Mockito.when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {25, 15, 5});
-    SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(dataServiceMock);
-    OptionalInt result = someBusinessImpl.findTheGreatestFromAllData();
+    OptionalInt result = businessImpl.findTheGreatestFromAllData();
     assertEquals(25, result.getAsInt());
   }
 
   @Test
-  void findTheGreatestFromAllData_oneValueTestScenario() {
-    DataService dataServiceMock = Mockito.mock(DataService.class);
+  void findTheGreatestFromAllData_oneValue() {
     Mockito.when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {35});
-    SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(dataServiceMock);
-    OptionalInt result = someBusinessImpl.findTheGreatestFromAllData();
+    OptionalInt result = businessImpl.findTheGreatestFromAllData();
     assertEquals(35, result.getAsInt());
+  }
+
+  @Test
+  void findTheGreatestFromAllData_EmptyArray() {
+    Mockito.when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
+    OptionalInt result = businessImpl.findTheGreatestFromAllData();
+    assertTrue(result.isEmpty());
   }
 }
