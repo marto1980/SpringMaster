cd53ca0794af72e92157d6f91fbaf567381ff1d2
Martin Ivanov <marto1980@gmail.com>
2026-06-19 14:24:16 +0300

    Create AOP Logging Aspect and Pointcut

---
+++ learn-spring-aop/src/main/java/com/marto/learn_spring_aop/aop_example/aspects/LoggingAspect.java
@@ -0,0 +1,19 @@
+package com.marto.learn_spring_aop.aop_example.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+public class LoggingAspect {
+  private Logger logger = LoggerFactory.getLogger(getClass());
+
+  @Before("execution(* com.marto.learn_spring_aop.aop_example.*.*.*(..))")
+  public void logMethodCall(JoinPoint joinPoint) {
+    logger.info("Before Aspect - Method is called - {}", joinPoint);
+  }
+}
