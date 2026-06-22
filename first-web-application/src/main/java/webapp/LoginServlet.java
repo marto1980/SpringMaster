12a915e90648040f00441c7ef1eb4c4770c52756
Martin Ivanov <marto1980@gmail.com>
2026-06-22 13:54:11 +0300

    Create LoginServlet from Scratch Again

--- first-web-application/src/main/java/webapp/LoginServlet.java
+++ first-web-application/src/main/java/webapp/LoginServlet.java
@@ -6,31 +6,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-
-/*
- * Browser sends Http Request to Web Server
- *
- * Code in Web Server => Input:HttpRequest, Output: HttpResponse
- * Jakarta EE with Servlets
- *
- * Web Server responds with Http Response
- */
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    PrintWriter out = response.getWriter();
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>Yahoo!!!!!!!!</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("My First Modern Jetty Servlet via Java 25!");
-    out.println("</body>");
-    out.println("</html>");
+    req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
   }
 }
---
+++ first-web-application/src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>From JSP</title>
+</head>
+<body>
+  My First JSP
+</body>
+</html>
