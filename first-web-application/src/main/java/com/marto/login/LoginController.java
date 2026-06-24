826e20bf140e7190bbd55d184e2c6378756c2391
Martin Ivanov <marto1980@gmail.com>
2026-06-24 16:52:59 +0300

    Spring Autowiring and Dependency Injection

--- first-web-application/src/main/java/com/marto/login/LoginController.java
+++ first-web-application/src/main/java/com/marto/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.marto.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-  private LoginService service = new LoginService();
+  @Autowired private LoginService service;
 
   @RequestMapping(value = "/login", method = RequestMethod.GET)
   public String showLoginPage() {
--- first-web-application/src/main/java/com/marto/login/LoginService.java
+++ first-web-application/src/main/java/com/marto/login/LoginService.java
@@ -1,5 +1,8 @@
 package com.marto.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 
   public boolean validateUser(String user, String password) {
